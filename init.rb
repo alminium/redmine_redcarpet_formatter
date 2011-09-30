4ea2ba1e2149f64a3d136f371cc932020f9723d1
mikoto20000 <mikoto20000@gmail.com>
2011-10-01 00:33:30 +0900

    update version 0.9.0.

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_redcarpet_formatter do
   name 'Redcarpet Markdown Wiki formatter'
   author 'Mikoto Misaka'
   description 'Markdown wiki formatting by Redcarpet for Redmine'
-  version '0.0.2'
+  version '0.9.0'
 
   wiki_format_provider 'markdown', RedmineRedcarpetFormatter::WikiFormatter, RedmineRedcarpetFormatter::Helper
 
