22ff967e81979a0a6ff2a3ddded50d418e2c6f0f
Hiroyuki MORITA <chiastolite.1980@gmail.com>
2012-01-07 12:51:06 +0900

    support 'Single section edit'.

--- lib/redmine_redcarpet_formatter/wiki_formatter.rb
+++ lib/redmine_redcarpet_formatter/wiki_formatter.rb
@@ -4,9 +4,9 @@ require 'redcarpet'
 class HTMLwithSyntaxHighlighting < Redcarpet::Render::HTML
   def block_code(code, language)
     if language != nil 
-    "<pre>\n<code class='#{language} syntaxhl'>" \
-      + Redmine::SyntaxHighlighting.highlight_by_language(code, language) \
-      + "</code>\n</pre>"
+      "<pre>\n<code class='#{language} syntaxhl'>" \
+        + Redmine::SyntaxHighlighting.highlight_by_language(code, language) \
+        + "</code>\n</pre>"
     else 
       "<pre>\n" + code + "</pre>"
     end
@@ -15,11 +15,10 @@ end
 
 module RedmineRedcarpetFormatter
   class WikiFormatter
-#    include ActionView::Helpers::TagHelper
-    
+    #    include ActionView::Helpers::TagHelper
+
     def initialize(text)
       @text = text
-
     end
 
     def to_html(&block)
@@ -28,8 +27,69 @@ module RedmineRedcarpetFormatter
     rescue => e
       return("<pre>problem parsing wiki text: #{e.message}\n"+
              "original text: \n"+
-             @text+
-             "</pre>")
+               @text+
+               "</pre>")
+    end
+
+    def get_section(index)
+      section = extract_sections(index)[1]
+      hash = Digest::MD5.hexdigest(section)
+      return section, hash
+    end
+
+    def update_section(index, update, hash=nil)
+      t = extract_sections(index)
+      if hash.present? && hash != Digest::MD5.hexdigest(t[1])
+        raise Redmine::WikiFormatting::StaleSectionError
+      end
+      t[1] = update unless t[1].blank?
+      t.reject(&:blank?).join "\n\n"
+    end
+
+    private
+
+    def extract_sections(index)
+      selected, before, after = [[],[],[]]
+      pre = :none
+      state = 'before'
+
+      selected_level = 0
+      header_count = 0
+
+      @text.each do |line|
+
+        if line =~ /^(~~~|```)/
+          pre = pre == :pre ? :none : :pre
+        elsif pre == :none
+          
+          level = if line =~ /^(#+)/
+                    $1.length
+                  elsif line.strip =~ /^=+$/ 
+                    line = eval(state).pop + line
+                    1
+                  elsif line.strip =~ /^-+$/ 
+                    line = eval(state).pop + line
+                    2
+                  else
+                    nil
+                  end
+          unless level.nil?
+            if level <= 4
+              header_count += 1
+              if state == 'selected' and selected_level >= level
+                state = 'after'
+              elsif header_count == index
+                state = 'selected'
+                selected_level = level
+              end
+            end
+          end
+        end
+
+        eval("#{state} << line")
+      end
+
+      [before, selected, after].map{|x| x.join.strip}
     end
   end
 end
---
+++ test/unit/redcarpet_formatter_test.rb
@@ -0,0 +1,147 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'digest/md5'
+
+class Redmine::WikiFormatting::RedcarpetFormatterTest < ActionView::TestCase
+
+  def setup
+    @formatter = RedmineRedcarpetFormatter::WikiFormatter
+  end
+
+  STR_WITHOUT_PRE = [
+  # 0
+"# Title
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas sed libero.",
+  # 1
+"## Heading 2
+
+Maecenas sed elit sit amet mi accumsan vestibulum non nec velit. Proin porta tincidunt lorem, consequat rhoncus dolor fermentum in.
+
+Cras ipsum felis, ultrices at porttitor vel, faucibus eu nunc.",
+  # 2
+"## Heading 2
+
+Morbi facilisis accumsan orci non pharetra.
+
+### Heading 3
+
+Nulla nunc nisi, egestas in ornare vel, posuere ac libero.",
+  # 3
+"### Heading 3
+
+Praesent eget turpis nibh, a lacinia nulla.",
+  # 4
+"## Heading 2
+
+Ut rhoncus elementum adipiscing."]
+
+  TEXT_WITHOUT_PRE = STR_WITHOUT_PRE.join("\n\n").freeze
+
+
+  STR_WITHOUT_PRE_ALTER = STR_WITHOUT_PRE.map{|x| 
+    x.split("\n").map do |line|
+      if line =~ /^([#]{1,2}) *([^#]+)$/
+        $2 + "\n" + ($1.length == 1 ? '===' : '---') + "\n"
+      else
+        line
+      end
+    end.join("\n")
+  }
+
+  TEXT_WITHOUT_PRE_ALTER = STR_WITHOUT_PRE_ALTER.join("\n\n").freeze
+
+  
+  def test_get_section_should_return_the_requested_section_and_its_hash
+    [[STR_WITHOUT_PRE, TEXT_WITHOUT_PRE],[STR_WITHOUT_PRE_ALTER, TEXT_WITHOUT_PRE_ALTER]].each do |items|
+      str, text = items
+      assert_section_with_hash str[1], text, 2
+      assert_section_with_hash str[2..3].join("\n\n"), text, 3
+      assert_section_with_hash str[3], text, 5
+      assert_section_with_hash str[4], text, 6
+
+      assert_section_with_hash '', text, 0
+      assert_section_with_hash '', text, 10
+    end
+  end
+
+  def test_update_section_should_update_the_requested_section
+    replacement = "New text"
+
+    assert_equal [STR_WITHOUT_PRE[0], replacement, STR_WITHOUT_PRE[2..4]].flatten.join("\n\n"), @formatter.new(TEXT_WITHOUT_PRE).update_section(2, replacement)
+    assert_equal [STR_WITHOUT_PRE[0..1], replacement, STR_WITHOUT_PRE[4]].flatten.join("\n\n"), @formatter.new(TEXT_WITHOUT_PRE).update_section(3, replacement)
+    assert_equal [STR_WITHOUT_PRE[0..2], replacement, STR_WITHOUT_PRE[4]].flatten.join("\n\n"), @formatter.new(TEXT_WITHOUT_PRE).update_section(5, replacement)
+    assert_equal [STR_WITHOUT_PRE[0..3], replacement].flatten.join("\n\n"), @formatter.new(TEXT_WITHOUT_PRE).update_section(6, replacement)
+
+    assert_equal TEXT_WITHOUT_PRE, @formatter.new(TEXT_WITHOUT_PRE).update_section(0, replacement)
+    assert_equal TEXT_WITHOUT_PRE, @formatter.new(TEXT_WITHOUT_PRE).update_section(10, replacement)
+  end
+
+  def test_update_section_with_hash_should_update_the_requested_section
+    replacement = "New text"
+
+    assert_equal [STR_WITHOUT_PRE[0], replacement, STR_WITHOUT_PRE[2..4]].flatten.join("\n\n"),
+      @formatter.new(TEXT_WITHOUT_PRE).update_section(2, replacement, Digest::MD5.hexdigest(STR_WITHOUT_PRE[1]))
+  end
+
+  def test_update_section_with_wrong_hash_should_raise_an_error
+    assert_raise Redmine::WikiFormatting::StaleSectionError do
+      @formatter.new(TEXT_WITHOUT_PRE).update_section(2, "New text", Digest::MD5.hexdigest("Old text"))
+    end
+  end
+
+  STR_WITH_PRE = [
+    # 0
+    "# Title
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas sed libero.",
+# 1
+"## Heading 2
+
+~~~ ruby
+  def foo
+  end
+~~~
+
+Morbi facilisis accumsan orci non pharetra.
+
+
+```
+Pre Content:
+
+## Inside pre
+
+<tag> inside pre block
+
+Morbi facilisis accumsan orci non pharetra.
+```",
+# 2
+"### Heading 3
+
+Nulla nunc nisi, egestas in ornare vel, posuere ac libero."]
+
+def test_get_section_should_ignore_pre_content
+  text = STR_WITH_PRE.join("\n\n")
+
+  assert_section_with_hash STR_WITH_PRE[1..2].join("\n\n"), text, 2
+  assert_section_with_hash STR_WITH_PRE[2], text, 3
+end
+
+def test_update_section_should_not_escape_pre_content_outside_section
+  text = STR_WITH_PRE.join("\n\n")
+  replacement = "New text"
+
+  assert_equal [STR_WITH_PRE[0..1], "New text"].flatten.join("\n\n"),
+    @formatter.new(text).update_section(3, replacement)
+end
+
+private
+
+def assert_section_with_hash(expected, text, index)
+  result = @formatter.new(text).get_section(index)
+
+  assert_kind_of Array, result
+  assert_equal 2, result.size
+  assert_equal expected, result.first, "section content did not match"
+  assert_equal Digest::MD5.hexdigest(expected), result.last, "section hash did not match"
+end
+end
