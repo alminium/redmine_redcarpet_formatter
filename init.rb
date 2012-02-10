d5358f0602d62a2cdf1f9ec22808625aa850774e
root <root@centos62.(none)>
2012-02-11 00:24:25 +0900

    fixed invalid version.

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_redcarpet_formatter do
   name 'Redcarpet Markdown Wiki formatter'
   author 'Mikoto Misaka'
   description 'Markdown wiki formatting by Redcarpet for Redmine'
-  version '1.0.0'
+  version '1.1.1'
 
   wiki_format_provider 'markdown', RedmineRedcarpetFormatter::WikiFormatter, RedmineRedcarpetFormatter::Helper
 
