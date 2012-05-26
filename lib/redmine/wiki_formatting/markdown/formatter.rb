e0bdc1e7ca6d23693f082865bd3e737a210a0697
mikoto20000 <mikoto20000@gmail.com>
2012-05-26 12:35:55 +0900

    Format code.

--- lib/redmine/wiki_formatting/markdown/formatter.rb
+++ lib/redmine/wiki_formatting/markdown/formatter.rb
@@ -7,8 +7,8 @@ class HTMLwithSyntaxHighlighting < Redcarpet::Render::HTML
   def block_code(code, language)
     if language != nil 
       "<pre>\n<code class='#{language} syntaxhl'>" \
-        + Redmine::SyntaxHighlighting.highlight_by_language(code, language) \
-        + "</code>\n</pre>"
+      + Redmine::SyntaxHighlighting.highlight_by_language(code, language) \
+      + "</code>\n</pre>"
     else 
       "<pre>\n" + CGI.escapeHTML(code) + "</pre>"
     end
@@ -19,86 +19,86 @@ class HTMLwithSyntaxHighlighting < Redcarpet::Render::HTML
 end  
 
 module Redmine
-module WikiFormatting
-module Markdown
-  class Formatter
-    #    include ActionView::Helpers::TagHelper
+  module WikiFormatting
+    module Markdown
+      class Formatter
+        #    include ActionView::Helpers::TagHelper
 
-    def initialize(text)
-      @text = text
-    end
-
-    def to_html(&block)
-      markdown = Redcarpet::Markdown.new(HTMLwithSyntaxHighlighting, :autolink => true, :space_after_headers => true,:fenced_code_blocks => true, :tables => true, :strikethrough => true, :superscript => true)
-      markdown.render(@text)
-    rescue => e
-      return("<pre>problem parsing wiki text: #{e.message}\n"+
-             "original text: \n"+
-               @text+
-               "</pre>")
-    end
-
-    def get_section(index)
-      section = extract_sections(index)[1]
-      hash = Digest::MD5.hexdigest(section)
-      return section, hash
-    end
-
-    def update_section(index, update, hash=nil)
-      t = extract_sections(index)
-      if hash.present? && hash != Digest::MD5.hexdigest(t[1])
-        raise Redmine::WikiFormatting::StaleSectionError
-      end
-      t[1] = update unless t[1].blank?
-      t.reject(&:blank?).join "\n\n"
-    end
-
-    private
-
-    def extract_sections(index)
-      selected, before, after = [[],[],[]]
-      pre = :none
-      state = 'before'
-
-      selected_level = 0
-      header_count = 0
-
-      @text.each do |line|
-
-        if line =~ /^(~~~|```)/
-          pre = pre == :pre ? :none : :pre
-        elsif pre == :none
-          
-          level = if line =~ /^(#+)/
-                    $1.length
-                  elsif line.strip =~ /^=+$/ 
-                    line = eval(state).pop + line
-                    1
-                  elsif line.strip =~ /^-+$/ 
-                    line = eval(state).pop + line
-                    2
-                  else
-                    nil
-                  end
-          unless level.nil?
-            if level <= 4
-              header_count += 1
-              if state == 'selected' and selected_level >= level
-                state = 'after'
-              elsif header_count == index
-                state = 'selected'
-                selected_level = level
-              end
-            end
-          end
+        def initialize(text)
+          @text = text
         end
 
-        eval("#{state} << line")
-      end
+        def to_html(&block)
+          markdown = Redcarpet::Markdown.new(HTMLwithSyntaxHighlighting, :autolink => true, :space_after_headers => true,:fenced_code_blocks => true, :tables => true, :strikethrough => true, :superscript => true)
+          markdown.render(@text)
+        rescue => e
+          return("<pre>problem parsing wiki text: #{e.message}\n"+
+                 "original text: \n"+
+                 @text+
+                 "</pre>")
+        end
 
-      [before, selected, after].map{|x| x.join.strip}
+        def get_section(index)
+          section = extract_sections(index)[1]
+          hash = Digest::MD5.hexdigest(section)
+          return section, hash
+        end
+
+        def update_section(index, update, hash=nil)
+          t = extract_sections(index)
+          if hash.present? && hash != Digest::MD5.hexdigest(t[1])
+            raise Redmine::WikiFormatting::StaleSectionError
+          end
+          t[1] = update unless t[1].blank?
+          t.reject(&:blank?).join "\n\n"
+        end
+
+        private
+
+        def extract_sections(index)
+          selected, before, after = [[],[],[]]
+          pre = :none
+          state = 'before'
+
+          selected_level = 0
+          header_count = 0
+
+          @text.each do |line|
+
+            if line =~ /^(~~~|```)/
+              pre = pre == :pre ? :none : :pre
+            elsif pre == :none
+              
+              level = if line =~ /^(#+)/
+                        $1.length
+                      elsif line.strip =~ /^=+$/ 
+                        line = eval(state).pop + line
+                        1
+                      elsif line.strip =~ /^-+$/ 
+                        line = eval(state).pop + line
+                        2
+                      else
+                        nil
+                      end
+              unless level.nil?
+                if level <= 4
+                  header_count += 1
+                  if state == 'selected' and selected_level >= level
+                    state = 'after'
+                  elsif header_count == index
+                    state = 'selected'
+                    selected_level = level
+                  end
+                end
+              end
+            end
+
+            eval("#{state} << line")
+          end
+
+          [before, selected, after].map{|x| x.join.strip}
+        end
+      end
     end
   end
-end
-end
 end
--- lib/redmine/wiki_formatting/markdown/helper.rb
+++ lib/redmine/wiki_formatting/markdown/helper.rb
@@ -1,44 +1,43 @@
 module Redmine
   module WikiFormatting
-            
-module Markdown
-  module Helper
-    unloadable
+    
+    module Markdown
+      module Helper
+        unloadable
 
-    def wikitoolbar_for(field_id)
-      heads_for_wiki_formatter
-      url = "#{::Redmine::Utils.relative_url_root}/plugin_assets/redmine_redcarpet_formatter/help/markdown_wiki_syntax.html"      
-      help_link = l(:setting_text_formatting) + ': ' +
-        link_to(l(:label_help), url,
-        :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=480, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
-      javascript_tag(<<-EOD);
+        def wikitoolbar_for(field_id)
+          heads_for_wiki_formatter
+          url = "#{::Redmine::Utils.relative_url_root}/plugin_assets/redmine_redcarpet_formatter/help/markdown_wiki_syntax.html"      
+          help_link = l(:setting_text_formatting) + ': ' +
+            link_to(l(:label_help), url,
+                    :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=480, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
+          javascript_tag(<<-EOD);
       var toolbar = new jsToolBar($('#{field_id}'));
       toolbar.setHelpLink('#{help_link}');
       toolbar.draw();
       EOD
-    end
-
-
-    def initial_page_content(page)
-      "#{page.pretty_title}\n#{'='*page.pretty_title.length}"
-    end
-
-
-    def heads_for_wiki_formatter
-      unless @heads_for_wiki_formatter_included
-        content_for :header_tags do
-          javascript_include_tag('jstoolbar/jstoolbar') +
-          javascript_include_tag('jstoolbar/redcarpet', :plugin => 'redmine_redcarpet_formatter') +
-          javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
-#          javascript_include_tag("lang/redcarpet-#{current_language.to_s.downcase}", :plugin => 'redmine_redcarpet_formatter') +
-          stylesheet_link_tag('jstoolbar') +
-          stylesheet_link_tag('html4css1', :plugin => 'redmine_redcarpet_formatter') +
-          stylesheet_link_tag('redcarpet', :plugin => 'redmine_redcarpet_formatter')
         end
-        @heads_for_wiki_formatter_included = true
+
+
+        def initial_page_content(page)
+          "#{page.pretty_title}\n#{'='*page.pretty_title.length}"
+        end
+
+
+        def heads_for_wiki_formatter
+          unless @heads_for_wiki_formatter_included
+            content_for :header_tags do
+              javascript_include_tag('jstoolbar/jstoolbar') +
+                javascript_include_tag('jstoolbar/redcarpet', :plugin => 'redmine_redcarpet_formatter') +
+                javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
+                stylesheet_link_tag('jstoolbar') +
+                stylesheet_link_tag('html4css1', :plugin => 'redmine_redcarpet_formatter') +
+                stylesheet_link_tag('redcarpet', :plugin => 'redmine_redcarpet_formatter')
+            end
+            @heads_for_wiki_formatter_included = true
+          end
+        end
       end
     end
   end
-end
-end
 end
